332421138c6c6001ff4aff9c99efd78791c685d0
ultrasound2k@gmail.com <Rokoreamc913>
2017-03-11 21:56:19 +0900

    add restapi

---
+++ restapi/views.py
@@ -0,0 +1,39 @@
+# Django REST framework
+from django.contrib.auth.models import User, Group
+
+from rest_framework import viewsets
+from rest_framework import generics
+
+from restapi.serializers import UserSerializer, GroupSerializer, SnippetSerializer
+from restapi.models import Snippet
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+class SnipptList(generics.ListCreateAPIView):
+    """
+    List all snippets, or create a new snippet.
+    """
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, update or delete a snippet instance.
+    """
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
